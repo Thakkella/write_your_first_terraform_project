984cbe8fe55d4cf9607c0aecb3da2fa5835b462a
Thakkella <jkrishna258@gmail.com>
2025-11-05 11:42:03 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -10,15 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
